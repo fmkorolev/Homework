ece6388b8f8ed6aa6ce1758f9098c3a6bf89c28d
filip korolev <filippok089@gmail.com>
2022-04-12 22:03:36 +0300

    first code made

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Задание 1
+/*
+int a;
+int b;
+int large;
+Console.Write("Input first number : ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+ b = Convert.ToInt32(Console.ReadLine());
+{
+if (a > b)
+large = a;
+else
+large = b;
+}
+
+Console.WriteLine("Largest number is {0}", large);
+
+Console.ReadLine();
+*/
+
